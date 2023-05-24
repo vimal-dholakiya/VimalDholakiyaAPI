93ca1de7d2882abbde7e117b9f337023ec2f8031
vimal-dholakiya <vimaldholakiya.yami@gmail.com>
2023-05-24 13:54:25 +0530

    start

--- app/src/main/java/com/vimalapi/MainActivity.java
+++ app/src/main/java/com/vimalapi/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.vimalapi.adsmodels.ConstantAds;
+import com.vimalapi.adutils.BaseAdsClass;
+
+public class MainActivity extends BaseAdsClass {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ConstantAds.setAdsURL("Sheet_ID");
+
     }
 }
